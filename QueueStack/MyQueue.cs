d4fc2bf424b71f5977d9d98218292f94c24f6a93
Ani Mkrtchyan <mkrtchyan.ani@outlook.com>
2022-11-29 15:34:46 +0400

    Add project files.

---
+++ QueueStack/MyQueue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueStack
+{
+    public class MyQueue<T> 
+    {
+
+        public Stack<T> inbox;
+        public Stack<T> outbox;
+        private int size;
+
+        public MyQueue()
+        {
+            inbox = new Stack<T>();
+            outbox = new Stack<T>();
+            
+        }
+
+        public void Enqueue(T item)
+        {
+            inbox.Push(item);
+            size++;
+        }
+
+        public T Dequeue()
+        {
+            if (outbox.Count == 0)
+            {
+                while (inbox.Count != 0)
+                {
+                    outbox.Push(inbox.Pop());
+
+                }
+            }
+            return outbox.Pop();
+        }
+        public int Size()
+        {
+            return size;
+        }
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+    }
+
+
+}
---
+++ QueueStack/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace QueueStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<int> queue = new MyQueue<int>();
+
+            // enqueue integers 1..3
+            for (int i = 1; i <= 3; i++)
+                queue.Enqueue(i);
+
+            // execute 2 dequeue operations 
+            for (int i = 0; i < 2; i++)
+                Console.WriteLine($"Dequeued: " + queue.Dequeue());
+
+            // enqueue integers 4..5
+            for (int i = 4; i <= 6; i++)
+                queue.Enqueue(i);
+
+            // dequeue the rest
+            while (!queue.isEmpty())
+                Console.WriteLine($"Dequeued: " + queue.Dequeue());
+        }
+    }
+}
